dc72397471f927dae60791a4eca54ff24bf589e0
liss <liss_shun@outlook.com>
2018-06-15 11:46:22 +0800

    WordCount 修改

--- src/main/java/com/liss/hadoop/WordCount.java
+++ src/main/java/com/liss/hadoop/WordCount.java
@@ -28,7 +28,7 @@ public class WordCount {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
-            LOG.info("map value:"+value.toString());
+//            LOG.info("map value:"+value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
@@ -55,6 +55,15 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         System.setProperty("hadoop.home.dir", "D:\\hadoop-2.6.4");
         Configuration conf = new Configuration();
+        conf.set("mapreduce.job.maps","2");
+        conf.set("mapreduce.job.reduces","2");
+        conf.set("mapreduce.input.fileinputformat.split.minsize","2");
+        conf.set("mapreduce.task.io.sort.mb","25");
+        conf.set("mapreduce.map.combine.minspills","3");
+        conf.set("mapreduce.reduce.memory.totalbytes","99614720");
+        conf.set("mapreduce.task.io.sort.factor","2");
+        conf.set("mapreduce.reduce.shuffle.input.buffer.percent","0.4");
+        conf.set("mapreduce.reduce.shuffle.memory.limit.percent","0.5");
         Job job = Job.getInstance(conf, "word count");
 //        job.setCacheFiles(null);
 //        job.setGroupingComparatorClass(null);
@@ -65,14 +74,15 @@ public class WordCount {
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+
         // 判断output文件夹是否存在，如果存在则删除
-        Path path = new Path("D:/GitWorkspace/hadoop_test/src/main/resources/stats");
+        Path path = new Path("C:\\Users\\Administrator\\Desktop\\sparkpartiton\\stats");
         FileSystem fileSystem = path.getFileSystem(conf);// 根据path找到这个文件
         if (fileSystem.exists(path)) {
             fileSystem.delete(path, true);// true的意思是，就算output有东西，也一带删除
         }
-        FileInputFormat.addInputPath(job, new Path("D:/GitWorkspace/hadoop_test/src/main/resources/WordCount.txt"));
-        FileOutputFormat.setOutputPath(job, new Path("D:/GitWorkspace/hadoop_test/src/main/resources/stats"));
+        FileInputFormat.addInputPath(job, new Path("C:\\Users\\Administrator\\Desktop\\sparkpartiton\\1987.txt"));
+        FileOutputFormat.setOutputPath(job, new Path("C:\\Users\\Administrator\\Desktop\\sparkpartiton\\stats"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
